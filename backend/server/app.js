118f8882c134e6fd80b4a183361dddade060d500
adriancolladomata <adriancolladomata@gmail.com>
2026-05-18 20:58:48 +0200

    Cambios para que vercel no de error CORS

--- backend/server/app.js
+++ backend/server/app.js
@@ -14,10 +14,23 @@ app.disable('x-powered-by')
 // Middlewares
 app.use(express.json())
 app.use(cookieParser())
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://fayver-app.vercel.app'
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Dinámico para producción
-  credentials: true
-}))
+  origin: function (origin, callback) {
+    // Permitir peticiones sin origen (como herramientas de testeo o Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'El origen CORS para este sitio no está permitido.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true // Vital porque usamos cookies/sesiones (withCredentials: true)
+}));
 
 // Rutas
 app.use('/auth', authRoutes)
